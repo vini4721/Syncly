b3c8f88ab440748cc754b27c8130128d54589f49
Vinayak Singh <vinayaksingh836@gmail.com>
2025-07-13 04:51:24 +0530

    made filter tabs active

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -12,12 +12,31 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   document.addEventListener("click", function (event) {
-    if (!showOptions.classList.contains("hidden") && !(showOptions.contains(event.target))) {
+    if (
+      !showOptions.classList.contains("hidden") &&
+      !showOptions.contains(event.target)
+    ) {
       showOptions.classList.toggle("hidden");
     }
   });
 
   document.querySelector(".logout").addEventListener("click", () => {
     window.location.href = "index.html";
+  });
+
+  const filterTabs = document.querySelectorAll(".filter-tab");
+
+  filterTabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      // Reset
+      filterTabs.forEach((ele) => {
+        ele.classList.remove("bg-purple-600", "text-white");
+        ele.classList.add("text-gray-400");
+      });
+
+      //Assign bg-color on active class
+      tab.classList.remove("text-gray-400");
+      tab.classList.add("bg-purple-600", "text-white");
+    });
   });
 });
